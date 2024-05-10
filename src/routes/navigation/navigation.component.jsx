363c537520d7668df47b4dd9c918aadc56e864f0
Edward Lopez <edtolo92@gmail.com>
2024-05-10 09:27:51 -0700

    added styling to Directory Products  Grid

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
-    <div>
-      <p>Navigation</p>
+    <Fragment>
+      <div
+        className="nav-container"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+          background: "black",
+          color: "white",
+        }}
+      >
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h2 style={{ color: "white" }}>LOGO</h2>
+        </Link>
+        <div className="nav-links">
+          <Link to="/auth" style={{ textDecoration: "none" }}>
+            <span style={{ color: "white" }}>SIGN IN</span>
+          </Link>
+          <Link to="/shop" style={{ textDecoration: "none" }}>
+            <span style={{ color: "white" }}>SHOP</span>
+          </Link>
+        </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          <h2 style={{ color: "white" }}>BUY</h2>
+        </Link>
+      </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
